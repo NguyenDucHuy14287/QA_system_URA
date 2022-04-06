67870bfec1f74e702c0411995f1c90f477cebf5c
NguyenDucHuy14287 <nguyenduchuy14287@gmail.com>
2022-04-06 22:55:41 +0700

    1

--- BART/torch_bart.py
+++ BART/torch_bart.py
@@ -51,16 +51,15 @@ for epoch in range(num_epochs):
         progress_bar.update(1)
 
 
-from datasets import load_metric
-metric = load_metric("accuracy")
-model.eval()
-for batch in eval_dataloader:
-    batch = {k: v.to(device) for k, v in batch.items()}
-    with torch.no_grad():
-        outputs = model(**batch)
+# from datasets import load_metric
+# metric = load_metric("accuracy")
+# model.eval()
+# for batch in eval_dataloader:
+#     batch = {k: v.to(device) for k, v in batch.items()}
+#     with torch.no_grad():
+#         outputs = model(**batch)
 
-    logits = outputs.logits
-    predictions = torch.argmax(logits, dim=-1)
-    metric.add_batch(predictions=predictions, references=batch["labels"])
+#     predictions = torch.argmax(logits, dim=-1)
+#     metric.add_batch(predictions=predictions, references=batch["labels"])
 
-metric.compute()
+# metric.compute()
