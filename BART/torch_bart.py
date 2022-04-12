4c774d6ba509caf6aa56231ffd116c1652e426f6
NguyenDucHuy14287 <nguyenduchuy14287@gmail.com>
2022-04-12 09:29:52 +0700

    1

--- BART/torch_bart.py
+++ BART/torch_bart.py
@@ -9,46 +9,46 @@ def pickle_load(file_path):
     file.close()
     return data
 
-############### Load data
-tokenized_datasets = pickle_load("tokenized_datasets")
-tokenized_datasets = tokenized_datasets.remove_columns(["answers", "context", "question"])
-tokenized_datasets.set_format("torch")
+# ############### Load data
+# tokenized_datasets = pickle_load("tokenized_datasets")
+# tokenized_datasets = tokenized_datasets.remove_columns(["answers", "context", "question"])
+# tokenized_datasets.set_format("torch")
 
-from torch.utils.data import DataLoader
-train_dataloader = DataLoader(tokenized_datasets, shuffle=True, batch_size=8)
-eval_dataloader = DataLoader(tokenized_datasets, batch_size=8)
+# from torch.utils.data import DataLoader
+# train_dataloader = DataLoader(tokenized_datasets, shuffle=True, batch_size=8)
+# eval_dataloader = DataLoader(tokenized_datasets, batch_size=8)
 
 ############### Load model
 model = pickle_load("model")
 
-from torch.optim import AdamW
-optimizer = AdamW(model.parameters(), lr=5e-5)
+# from torch.optim import AdamW
+# optimizer = AdamW(model.parameters(), lr=5e-5)
 
-from transformers import get_scheduler
-num_epochs = 3
-num_training_steps = num_epochs * len(train_dataloader)
-lr_scheduler = get_scheduler(
-    name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
-)
+# from transformers import get_scheduler
+# num_epochs = 3
+# num_training_steps = num_epochs * len(train_dataloader)
+# lr_scheduler = get_scheduler(
+#     name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
+# )
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-model.to(device)
+# device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# model.to(device)
 
-from tqdm.auto import tqdm
-progress_bar = tqdm(range(num_training_steps))
+# from tqdm.auto import tqdm
+# progress_bar = tqdm(range(num_training_steps))
 
-model.train()
-for epoch in range(num_epochs):
-    for batch in train_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        outputs = model(**batch)
-        loss = outputs.loss
-        loss.backward()
+# model.train()
+# for epoch in range(num_epochs):
+#     for batch in train_dataloader:
+#         batch = {k: v.to(device) for k, v in batch.items()}
+#         outputs = model(**batch)
+#         loss = outputs.loss
+#         loss.backward()
 
-        optimizer.step()
-        lr_scheduler.step()
-        optimizer.zero_grad()
-        progress_bar.update(1)
+#         optimizer.step()
+#         lr_scheduler.step()
+#         optimizer.zero_grad()
+#         progress_bar.update(1)
 
 
 tokenizer = pickle_load("tokenizer")
